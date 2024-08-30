4feab2587ce405848d15b355c2eb5ec55f73dc1b
sakshi-bhujbal <bhujbalsakshi510@gmail.com>
2024-08-30 11:49:45 +0530

    commit

---
+++ pathModule.js
@@ -0,0 +1,8 @@
+const path=require('path')
+let ext=path.extname('osModule.js')
+console.log(ext)
+let baseName=path.basename('E:\\College Material\\NodeJs\\f1.txt')
+console.log(baseName)
+
+console.log(__filename) //file path
+console.log(__dirname)  //directory path
