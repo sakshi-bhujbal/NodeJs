68fb7f52dd6e1346e038b1bf67f48b9dfa439e5f
sakshi-bhujbal <bhujbalsakshi510@gmail.com>
2024-09-01 16:22:00 +0530

    commit

--- 3_Express/app.js
+++ 3_Express/app.js
@@ -3,4 +3,9 @@ const app=express()
 app.get('/about',(req,res)=>{
 res.send('Hello from sakshi')
 })
+
+app.get('/courses/:id',(req,res)=>{
+    console.log(req.params)
+    res.send(req.params.id)
+})
 app.listen(3000,()=>console.log('Port is running on 3000'))
